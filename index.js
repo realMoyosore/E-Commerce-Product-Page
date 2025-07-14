2e42a559b811cf5cb10f544edf66462b3249c11c
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2025-07-14 23:40:14 +0100

    cart window updated

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const cartIcon = document.getElementById('cart-icon');
 const cartWindow = document.getElementsByClassName('cart-window');
 
 cartIcon.addEventListener('click', () => {
-    cartWindow.classList.toggle('hidden');
+    cartWindow.classList.toggle('visible');
 });
