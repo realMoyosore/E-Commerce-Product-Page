34da3d554bd80b4a3fabc23dc777556732fc28e7
Moyosore Sulola <moyixxoreoluwa@gmail.com>
2025-08-26 17:28:40 -0600

    further on cart

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ const cartWindow = document.querySelector('.cart-window');
 cartIcon.addEventListener('click', () => {
     cartWindow.classList.toggle('visible');
 });
+
+const closeCartBtn = document.getElementById('close-cart');
+closeCartBtn.addEventListener('click', () => {
+    cartWindow.classList.remove('visible');
+});
